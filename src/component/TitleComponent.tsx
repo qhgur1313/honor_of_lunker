a4b2a6f63f4cf49a041ec6f18f4218d8142678b4
bohyuk_lee <snumse1313@gmail.com>
2021-07-03 12:02:48 +0900

    1st commit

---
+++ src/component/TitleComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TitleComponent() {
+    const name: string = "스너퍼 런커의 전당";
+    return (
+        <div>
+            <h1> {name} </h1>
+        </div>
+    )
+}
+
+export default TitleComponent;
